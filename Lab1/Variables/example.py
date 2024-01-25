a04cf0650330dc59e8a722cd4ac7abaf4d26d76c
Sake2 <k_samat@kbtu.kz>
2024-01-25 10:39:17 +0600

    Add example

---
+++ Lab1/Variables/example.py
@@ -0,0 +1,100 @@
+x = 5
+y = "John"
+print(x) 
+print(y)
+
+
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+
+#Legal_variables
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+
+x = "Python is awesome"
+print(x)
+
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+
+x = 5
+y = 10
+print(x + y)
+
+
+x = 5
+y = "John"
+print(x, y)
+
+
+x = "Python is awesome"
+print(x)
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+
+x = 5
+y = 10
+print(x + y)
+
+
+x = 5
+y = "John"
+print(x, y)
+
+
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
+
+
+x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
+
