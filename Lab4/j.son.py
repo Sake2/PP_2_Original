e6252ae55ef70bf7669a4a22c69b33da866bddba
Sake2 <k_samat@kbtu.kz>
2024-03-01 09:51:06 +0600

    Lab5

--- Lab4/j.son.py
+++ Lab4/j.son.py
@@ -1,17 +1,17 @@
 import json
-with open('sample-data.json') as file:
+with open('Lab4/sample-data.json') as file:
     data = json.load(file)
 
 interface_status = data['imdata']
 
 print("Interface Status")
-print("=" * 80)
-print("{:<50} {:<20} {:<8} {:<6}".format("DN", "Description", "Speed", "MTU"))
-print("-" * 80)
+print("=" * 85)
+print("DN"," "*48 ,"Description" ," "*7, "Speed" ," "*3, "MTU")
+print("-" * 51 ,"-"*19,"-"*9,"-"*6 )
 
 for entry in interface_status:
-    dn = entry['fvCEp']['attributes']['dn']
-    description = entry['fvCEp']['attributes'].get('descr', '')
-    speed = entry['fvCEp']['attributes'].get('speed', 'inherit')
-    mtu = entry['fvCEp']['attributes'].get('mtu', '')
-    print("{:<50} {:<20} {:<8} {:<6}".format(dn, description, speed, mtu))
+    dn = entry["l1PhysIf"]['attributes']['dn']
+    description = entry["l1PhysIf"]['attributes'].get('descr', '')
+    speed = entry["l1PhysIf"]['attributes'].get('speed', 'inherit')
+    mtu = entry["l1PhysIf"]['attributes'].get('mtu', '')
+    print(dn , description," "*27 , speed," ", mtu)
